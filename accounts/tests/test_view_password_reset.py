90e6b513dd2bfad217f3754492225fd71e30621c
pralhad88 <pralhad18@navgurukul.org>
2018-09-27 16:10:37 +0530

    Initial commit

---
+++ accounts/tests/test_view_password_reset.py
@@ -0,0 +1,16 @@
+from django.contrib.auth import views as auth_views
+from django.core.urlresolvers import reverse
+from django.urls import resolve
+from django.test import TestCase
+
+class PasswordResetCompleteTests(TestCase):
+    def setUp(self):
+        url = reverse('password_reset_complete')
+        self.response = self.client.get(url)
+
+    def test_status_code(self):
+        self.assertEquals(self.response.status_code, 200)
+
+    def test_view_function(self):
+        view = resolve('/reset/complete/')
+        self.assertEquals(view.func.view_class, auth_views.PasswordResetCompleteView)
